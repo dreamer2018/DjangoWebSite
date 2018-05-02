390a6cc36f15f11b52570c42dccfba21de2ef843
dreamer2018 <zhoupans_mail@163.com>
2018-05-03 01:02:37 +0800

    add

--- FileUpLoad/views.py
+++ FileUpLoad/views.py
@@ -4,10 +4,11 @@
     Created by ZhouPan at 2018/5/2.
 """
 
-
 from django.shortcuts import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import os
+import time
+
 # Create your views here.
 
 
@@ -18,10 +19,12 @@ def upload(request):
         return HttpResponse('Successful')  # 此处简单返回一个成功的消息，在实际应用中可以返回到指定的页面中
 
 
-def handle_upload_file(file, filename):
+def handle_upload_file(file_data, file_name):
+    extension = os.path.splitext(file_name)[1]
+    filename = "%d%s" % (int(round(time.time()*1000)), extension)
     path = 'FileUpLoad/Data/'  # 上传文件的保存路径，可以自己指定任意的路径
     if not os.path.exists(path):
         os.makedirs(path)
     with open(path + filename, 'wb+') as destination:
-        for chunk in file.chunks():
+        for chunk in file_data.chunks():
             destination.write(chunk)
